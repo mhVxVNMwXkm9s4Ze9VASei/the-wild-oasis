69958e00175fca658a07f4288d84eba5af865455
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-10-09 22:53:11 -0500

    Created the App Layout
    This is coming out nicely... Also learned a trick for when I create my own layouts!
    
    Grids are cool.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
 	return (
@@ -15,39 +16,41 @@ function App() {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
-					<Route
-						index
-						element={
-							<Navigate
-								replace
-								to="dashboard"
-							/>
-						}
-					/>
-					<Route
-						path="dashboard"
-						element={<Dashboard />}
-					/>
-					<Route
-						path="bookings"
-						element={<Bookings />}
-					/>
-					<Route
-						path="cabins"
-						element={<Cabins />}
-					/>
-					<Route
-						path="users"
-						element={<Users />}
-					/>
-					<Route
-						path="Settings"
-						element={<Settings />}
-					/>
-					<Route
-						path="Account"
-						element={<Account />}
-					/>
+					<Route element={<AppLayout />}>
+						<Route
+							index
+							element={
+								<Navigate
+									replace
+									to="dashboard"
+								/>
+							}
+						/>
+						<Route
+							path="dashboard"
+							element={<Dashboard />}
+						/>
+						<Route
+							path="bookings"
+							element={<Bookings />}
+						/>
+						<Route
+							path="cabins"
+							element={<Cabins />}
+						/>
+						<Route
+							path="users"
+							element={<Users />}
+						/>
+						<Route
+							path="Settings"
+							element={<Settings />}
+						/>
+						<Route
+							path="Account"
+							element={<Account />}
+						/>
+					</Route>
 					<Route
 						path="Login"
 						element={<Login />}
