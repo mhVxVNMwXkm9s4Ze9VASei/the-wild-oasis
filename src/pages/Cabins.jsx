009bce2787ad3f881133515ca81701f9025f053f
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-10-12 22:09:31 -0500

    Installed React Hook Form
    Added it to this form... 🤔

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+	const [showForm, setShowForm] = useState(false);
 	return (
 		<>
 			<Row type="horizontal">
@@ -11,6 +15,8 @@ function Cabins() {
 			</Row>
 			<Row>
 				<CabinTable />
+				<Button onClick={() => setShowForm(!showForm)}>Add new cabin</Button>
+				{showForm && <CreateCabinForm />}
 			</Row>
 		</>
 	);
