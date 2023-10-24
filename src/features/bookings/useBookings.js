438ac62001686bd91783f7fadc93c108a7168338
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-10-24 00:06:29 -0500

    Added ability to sort Bookings.
    Still not sure why the table operations buttons aren't working... 🤔

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,19 +5,25 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
 	const [searchParams] = useSearchParams();
 
+	// Filter.
 	const filterValue = searchParams.get("status");
 	const filter =
 		!filterValue || filterValue === "all"
 			? null
 			: { field: "status", value: filterValue };
 
+	//Sort
+	const sortByRaw = searchParams.get("sortBy") || "start_date-desc";
+	const [field, direction] = sortByRaw.split("-");
+	const sortBy = { direction, field };
+
 	const {
 		isLoading,
 		data: bookings,
 		error,
 	} = useQuery({
-		queryKey: ["bookings", filter],
-		queryFn: () => getBookings({ filter }),
+		queryKey: ["bookings", filter, sortBy],
+		queryFn: () => getBookings({ filter, sortBy }),
 	});
 
 	return { bookings, error, isLoading };
