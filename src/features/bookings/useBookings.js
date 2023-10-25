2659ecec8b5ed82fb15bb8e5d6145b6afe32f770
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-10-25 00:00:11 -0500

    Added ability to prefetch pages.
    This is pretty cool. :scream_cat::scream_cat::scream_cat:

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { RESULTS_PER_PAGE } from "../../utils/constants";
 
 export function useBookings() {
+	const queryClient = useQueryClient();
 	const [searchParams] = useSearchParams();
 
 	// Filter.
@@ -20,6 +22,7 @@ export function useBookings() {
 	// Pagination.
 	const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+	// Query.
 	const {
 		isLoading,
 		data: { count, data: bookings } = {},
@@ -28,6 +31,21 @@ export function useBookings() {
 		queryKey: ["bookings", filter, page, sortBy],
 		queryFn: () => getBookings({ filter, page, sortBy }),
 	});
+
+	// Prefetching.
+	const pageCount = Math.ceil(count / RESULTS_PER_PAGE);
+
+	if (page < pageCount)
+		queryClient.prefetchQuery({
+			queryKey: ["bookings", filter, page + 1, sortBy],
+			queryFn: () => getBookings({ filter, page: page + 1, sortBy }),
+		});
+
+	if (page > 1)
+		queryClient.prefetchQuery({
+			queryKey: ["bookings", filter, page - 1, sortBy],
+			queryFn: () => getBookings({ filter, page: page - 1, sortBy }),
+		});
 
 	return { bookings, count, error, isLoading };
 }
