c63ab723ead6611eb6ae571a7801e0beb313840e
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-10-26 21:59:19 -0500

    Added the ability to check in a booking.
    I think I'm starting to understand the issue with styled components and props, though I can't find the commit in which I was complaining about it the other day... Strange!

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -10,7 +10,7 @@ export function useBooking() {
 		data: booking,
 		error,
 	} = useQuery({
-		queryKey: ["booking"],
+		queryKey: ["booking", booking_id],
 		queryFn: () => getBooking(booking_id),
 		retry: false,
 	});
